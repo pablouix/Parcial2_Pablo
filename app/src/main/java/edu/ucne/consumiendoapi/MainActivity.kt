8b41b2419cc3d275aadc31d72ffde7463475f964
pablouix <pablouix@gmail.com>
2022-11-02 19:18:04 -0400

    Actualizando Imagenes

--- app/src/main/java/edu/ucne/consumiendoapi/MainActivity.kt
+++ app/src/main/java/edu/ucne/consumiendoapi/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -13,6 +14,8 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -50,12 +53,12 @@ fun Greeting(name: String) {
 fun Mostrar(){
     
     Column(modifier = Modifier.fillMaxWidth()) {
-        Text(text = "Lista de imagenes")
-        LazyColumn(){
+        Text(text = "Lista de imagenes", fontSize = 20.sp)
+        LazyColumn( modifier = Modifier.padding(8.dp)){
             item {
                 AsyncImage(
                     modifier = Modifier.fillMaxSize(),
-                    model = "https://catequesisellimon.files.wordpress.com/2018/03/doc.jpg",
+                    model = "https://cdn.pixabay.com/photo/2020/04/12/07/18/post-it-5033079__480.jpg",
                     contentDescription = null
                 )
             }
@@ -69,21 +72,21 @@ fun Mostrar(){
             item {
                 AsyncImage(
                     modifier = Modifier.fillMaxSize(),
-                    model = "https://catequesisellimon.files.wordpress.com/2018/03/doc.jpg",
+                    model = "https://ci2.ual.es/wp-content/uploads/citar.png",
                     contentDescription = null
                 )
             }
             item {
                 AsyncImage(
                     modifier = Modifier.fillMaxSize(),
-                    model = "https://catequesisellimon.files.wordpress.com/2018/03/doc.jpg",
+                    model = "https://static.diariofemenino.com/uploads/amor/174578-mensajes-todos.jpg",
                     contentDescription = null
                 )
             }
             item {
                 AsyncImage(
                     modifier = Modifier.fillMaxSize(),
-                    model = "https://catequesisellimon.files.wordpress.com/2018/03/doc.jpg",
+                    model = "https://image.shutterstock.com/image-vector/forgetful-persons-brain-dementia-260nw-1880082937.jpg",
                     contentDescription = null
                 )
             }
