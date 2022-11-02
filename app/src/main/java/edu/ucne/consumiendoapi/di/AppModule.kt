3b12b9a35aaaefb10c0b507a7cf0bc816ec6434c
pablouix <pablouix@gmail.com>
2022-11-02 18:43:49 -0400

    app module

---
+++ app/src/main/java/edu/ucne/consumiendoapi/di/AppModule.kt
@@ -0,0 +1,43 @@
+package edu.ucne.consumiendoapi.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Singleton
+    @Provides
+    fun provideMoshi(): Moshi
+    {
+        return Moshi
+            .Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+    }
+
+
+    @Singleton
+    @Provides
+    fun provideSagApi(moshi: Moshi): VerbosApi {
+        val BASE_URL = "https://private-a127e-verbos.apiary-mock.com/verbos"
+
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(VerbosApi::class.java)
+    }
+
+
+
+
+}
