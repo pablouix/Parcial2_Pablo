e3a4b87e8552ed8ad13810935462e449289087c8
pablouix <pablouix@gmail.com>
2022-11-02 19:02:53 -0400

    View model

--- app/src/main/java/edu/ucne/consumiendoapi/ViewModelVerbos.kt
+++ app/src/main/java/edu/ucne/consumiendoapi/ViewModelVerbos.kt
@@ -1,2 +1,19 @@
 package edu.ucne.consumiendoapi
 
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import edu.ucne.consumiendoapi.Repositorio.ApiRepositorioVerbos
+import edu.ucne.consumiendoapi.remota.dto.VerbosApi
+import javax.inject.Inject
+
+@HiltViewModel
+class ViewModelVerbos @Inject constructor(
+    val repositorio: ApiRepositorioVerbos
+): ViewModel()
+{
+    init {
+
+
+
+    }
+}
