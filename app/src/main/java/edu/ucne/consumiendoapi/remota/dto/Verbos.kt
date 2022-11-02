d4ee07600034afa110cdb91c0bb66377e526f247
pablouix <pablouix@gmail.com>
2022-11-02 18:35:23 -0400

    class verbo

---
+++ app/src/main/java/edu/ucne/consumiendoapi/remota/dto/Verbos.kt
@@ -0,0 +1,8 @@
+package edu.ucne.consumiendoapi.remota.dto
+
+data class Verbos(
+    val Verbo: String="",
+    val Categoria: String = "",
+    val Nivel: String = "",
+    val Imagen: String =""
+)
